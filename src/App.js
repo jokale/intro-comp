4170b4f5edbf14bfe8b03a68ff37dbd5066758e3
jokale <kalemajoanna@gmail.com>
2021-01-11 11:36:33 +0000

    add all main components of the into sign up page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,24 @@ class App extends React.Component {
   render(){
     return (
     <div className="App">
-    
+    <h1>Learn to code by <br></br> watching others</h1>
+    <p>See how other experienced developers solve problems in real time.</p>
+    <p>Watching scripted tutorials is great, but understanding how</p>
+    <p>developers think is invaluable.</p>
+    <div id="purple">
+      <p>Try it for 7 days</p>
+      <p>then $20/mo. thereafter</p>
+    </div>
+
+    <div id="form">
+      <form>
+        <input placeholder="First Name"></input>
+        <input placeholder="Last Name"></input>
+        <input placeholder="Email Address"></input>
+        <input  type="password"placeholder="Password"></input>
+      </form>
+      <p>By clicking here you are agreeing to our terms and conditions </p>
+    </div>
     </div>
   );
   }
